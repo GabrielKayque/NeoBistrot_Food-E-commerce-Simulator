3aadf6eafeccaff840925fcc7b697f99c91bad9a
GabrielKayque <mr.biiiel@hotmail.com>
2023-08-07 11:27:46 -0300

    force type in useSnacks

--- src/services/useSnacks.ts
+++ src/services/useSnacks.ts
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import api from "./api";
+import { SnacksProps } from "./interfaces";
 
 export default function useSnacks(snacktype: string) {
 
-  const [snacks, setSnacks] = useState([]);
+  const [snacks, setSnacks] = useState<SnacksProps[]>([]);
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
